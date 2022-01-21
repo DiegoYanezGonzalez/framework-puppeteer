8b438e8931ef5c5cb93c41f0236ae654920edd6d
diego <diego.y8@gmail.com>
2022-01-21 16:05:01 -0300

    ProjectFrameWorkCreateBasePagePom

---
+++ __tests__/example.test.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer')
+
+describe('Test Puppeteer',()=>{
+
+    it('Debe abrir y cerrar el navegador',async ()=>{
+        const browser = await puppeteer.launch({
+            headless: false
+    
+          })
+          const page = await browser.newPage()
+          await page.goto('http://google.com/')
+          await page.waitForTimeout(10000)
+          await browser.close()
+    },30000)
+})
